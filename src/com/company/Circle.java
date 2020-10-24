5e1b0db7ac6503822c724810ca084fa49d1ea6e9
nazlikok <nazlikok@hotmail.com>
2020-10-25 01:18:44 +0300

    3.rd

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,14 +1,26 @@
 package com.company;
 
 public class Circle {
-    void Calculate(float radius){
+    int circle_x;
+    int circle_y;
 
-        float area,circum;
+    void calculateArea(float radius){
+        float area;
         area = (float) (Math.PI*radius*radius);
-        circum = (float) (2*Math.PI*radius);
-
         System.out.println("Area = " + area);
-        System.out.println("Circumference = " + circum);
     }
 
+    void calculateCircumference(float radius){
+        float circum;
+        circum = (float) (2*Math.PI*radius);
+        System.out.println("Circumference = " + circum);
+    }
+    void isInside(int point_x, int point_y, float radius, int circle_x, int circle_y) {
+        if ((point_x - circle_x) * (point_x - circle_x) + (point_y - circle_y) * (point_y - circle_y) <= radius * radius){
+            System.out.println("point is inside the circle");
+        }
+        else{
+            System.out.println("point is not inside the circle");
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,23 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        Circle Testcircle = new Circle();
+        Testcircle.circle_x=3;
+        Testcircle.circle_y=3;
+        int Point_x=5;
+        int Point_y=7;
+
+        Scanner myObj = new Scanner(System.in);
         System.out.println("Write a Raduis of Circle:");
 
-        float radius = myObj.nextFloat();  // Read user input
+        float radius = myObj.nextFloat();
         System.out.println("Raduis of Circle = " + radius);
 
-        Circle circle = new Circle();
-        circle.Calculate(radius);
-
+        Testcircle.calculateCircumference(radius);
+        Testcircle.calculateArea(radius);
+        Testcircle.isInside(Point_x,Point_y,radius,Testcircle.circle_x, Testcircle.circle_y);
     }
 }
